b18beb67d40dc383c2d251a31fe8aaeb5b82c834
Hossein <hosseinrouhi79@gmail.com>
2023-07-13 19:01:00 +0330

    create changeMessage method

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ new Vue({
     el: '#app',
     data: {
       message: 'Hello World'
+    },
+    methods: {
+      changeMessage: function(event){
+        this.message = event.target.value;
+      }
     }
   });
   
