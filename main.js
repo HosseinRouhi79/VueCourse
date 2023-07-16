e3d41b916b59000a8a388e018e79a4900eeba491
Hossein <hosseinrouhi79@gmail.com>
2023-07-16 23:42:00 +0330

    work with v-html

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
       message: 'Hello World',
       title: 'Vue.js',
-      link: 'https://google.com'
+      link: 'https://google.com',
+      otherLink:'<a href="https://google.com">Google2</a>'
     },
     methods: {
       changeMessage: function(event){
