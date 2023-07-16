d35e6a5c9fed0f3929cad279fb01b995f75238c7
Hossein <hosseinrouhi79@gmail.com>
2023-07-16 23:21:08 +0330

    work with v-bind

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 new Vue({
     el: '#app',
     data: {
-      message: 'Hello World'
+      message: 'Hello World',
+      title: 'Vue.js',
+      link: 'https://google.com'
     },
     methods: {
       changeMessage: function(event){
         this.message = event.target.value;
       },
       sayHello: function(){
-        return "Hello!"
+        // return "Hello!"
+        return this.title
       }
     }
   });
